54e5830a1cf95ad2a70d954a164a638c700442f3
Yaminibotta <bottayamini200@gmail.com>
2025-06-15 21:30:57 +0530

    Add files via upload

---
+++ import java.security.java
@@ -0,0 +1,50 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+    private static final String ALL_CHARS = UPPER + LOWER + DIGITS + SPECIAL;
+
+    private static SecureRandom random = new SecureRandom();
+
+    public static String generatePassword(int length) {
+        if (length < 4) {
+            throw new IllegalArgumentException("Password length should be at least 4");
+        }
+
+        StringBuilder password = new StringBuilder();
+
+        // Ensure at least one character from each category
+        password.append(UPPER.charAt(random.nextInt(UPPER.length())));
+        password.append(LOWER.charAt(random.nextInt(LOWER.length())));
+        password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+password.append(SPECIAL.charAt(random.nextInt(SPECIAL.length())));
+
+        // Fill the remaining characters
+        for (int i = 4; i < length; i++) {
+            password.append(ALL_CHARS.charAt(random.nextInt(ALL_CHARS.length())));
+        }
+
+        // Shuffle the characters
+        return shuffleString(password.toString());
+    }
+
+    private static String shuffleString(String input) {
+        char[] characters = input.toCharArray();
+        for (int i = characters.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            char temp = characters[i];
+            characters[i] = characters[j];
+            characters[j] = temp;
+        }
+        return new String(characters);
+    }
+
+    public static void main(String[] args) {
+        int length = 12; // Change password length as needed
+        String password = generatePassword(length);
+        System.out.println("Generated Password: " + password);
+    }
+}
